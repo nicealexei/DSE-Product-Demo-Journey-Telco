a8368d8b952f6fa4544b04ebf30d88f74da9910e
nicealexei <alexei.frumkin@nice.com>
2024-07-16 17:07:20 -0400

    modifications

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,7 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('my-cache').then(function(cache) {
         return cache.addAll([
-          '/',
-          '/index.html',
-          '/app.js',
-          '/style.css',
+          '/',          
         ]);
       })
     );
