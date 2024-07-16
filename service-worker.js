90905d23d5a06ad98f0c1c3cbe843778ffbddac9
nicealexei <alexei.frumkin@nice.com>
2024-07-16 16:33:22 -0400

    modifications

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('my-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/app.js',
+          '/style.css',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
