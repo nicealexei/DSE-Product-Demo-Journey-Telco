d58480aa39c5b3177cf67ff64349a89c89277834
nicealexei <alexei.frumkin@nice.com>
2024-07-17 16:44:50 -0400

    adding google firebase

--- service-worker.js
+++ service-worker.js
@@ -6,17 +6,40 @@ self.addEventListener('fetch', function(event) {
   // The service worker is fetching resources.
 });
 
-self.addEventListener('push', function(event) {
-  // Show a notification every 5 minutes
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [];
   event.waitUntil(
-    self.registration.showNotification('Telco PWA', {
-      body: 'This is a mobile push notification from Telco PWA. Have a great day!',
-      icon: '%PUBLIC_URL%/assets/images/assets/images/logo192.png'
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
 
-// Set an interval to dispatch push events every 5 minutes
-setInterval(() => {
-  self.dispatchEvent(new PushEvent('push'));
-}, 300000); // 300000 milliseconds = 5 minutes
+const firebaseConfig = {
+  apiKey: "AIzaSyDyjZ_hPS5f4MN2d8iqkUQdV7w0hFNpPko",
+  authDomain: "sendnotifications-fd4ab.firebaseapp.com",
+  projectId: "sendnotifications-fd4ab",
+  storageBucket: "sendnotifications-fd4ab.appspot.com",
+  messagingSenderId: "188964222220",
+  appId: "1:188964222220:web:d2fc465b9148a8d07ac529"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '%PUBLIC_URL%/assets/images/assets/images/logo192.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
