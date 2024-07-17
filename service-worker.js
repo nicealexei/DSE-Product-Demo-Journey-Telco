e99667ca29efd1f78c75b76d133ce93bfe911e37
nicealexei <alexei.frumkin@nice.com>
2024-07-17 11:23:34 -0400

    modifications

--- service-worker.js
+++ service-worker.js
@@ -5,3 +5,18 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   // The service worker is fetching resources.
 });
+
+self.addEventListener('push', function(event) {
+  // Show a notification every 5 minutes
+  event.waitUntil(
+    self.registration.showNotification('Telco PWA', {
+      body: 'This is a mobile push notification from Telco PWA. Have a great day!',
+      icon: '%PUBLIC_URL%/assets/images/assets/images/logo192.png'
+    })
+  );
+});
+
+// Set an interval to dispatch push events every 5 minutes
+setInterval(() => {
+  self.dispatchEvent(new PushEvent('push'));
+}, 300000); // 300000 milliseconds = 5 minutes
