edf04ce2d9ee2929b89fe70d0c915672f06b3c43
nicealexei <alexei.frumkin@nice.com>
2024-07-17 17:58:30 -0400

    A

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,32 @@
-import { initializeApp } from 'firebase/app';
-import { getMessaging, onBackgroundMessage } from 'firebase/messaging/sw';
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging.js');
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDyjZ_hPS5f4MN2d8iqkUQdV7w0hFNpPko",
+  authDomain: "sendnotifications-fd4ab.firebaseapp.com",
+  projectId: "sendnotifications-fd4ab",
+  storageBucket: "sendnotifications-fd4ab.appspot.com",
+  messagingSenderId: "188964222220",
+  appId: "1:188964222220:web:d2fc465b9148a8d07ac529"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+// Handle background messages
+messaging.onBackgroundMessage((payload) => {
+    console.log('Received background message ', payload);
+
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '%PUBLIC_URL%/assets/images/logo192.png'
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
 
 self.addEventListener('install', function(event) {
   // The service worker is installing.
@@ -22,30 +49,4 @@ self.addEventListener('activate', event => {
       );
     })
   );
-});
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDyjZ_hPS5f4MN2d8iqkUQdV7w0hFNpPko",
-  authDomain: "sendnotifications-fd4ab.firebaseapp.com",
-  projectId: "sendnotifications-fd4ab",
-  storageBucket: "sendnotifications-fd4ab.appspot.com",
-  messagingSenderId: "188964222220",
-  appId: "1:188964222220:web:d2fc465b9148a8d07ac529"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
-
-// Handle background messages
-onBackgroundMessage(messaging, (payload) => {
-    console.log('Received background message ', payload);
-
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: '%PUBLIC_URL%/assets/images/logo192.png'
-    };
-
-    self.registration.showNotification(notificationTitle, notificationOptions);
 });
