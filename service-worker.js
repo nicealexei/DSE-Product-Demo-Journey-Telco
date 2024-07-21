688c45bed346d4548c052b28ffbab47379ee4993
nicealexei <alexei.frumkin@nice.com>
2024-07-21 12:48:26 -0400

    A

--- service-worker.js
+++ service-worker.js
@@ -54,19 +54,31 @@ self.addEventListener('fetch', function(event) {
 });
 
 self.addEventListener('notificationclick', function(event) {
-  event.notification.close(); 
+  event.notification.close();
   event.waitUntil(
       clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
           for (var i = 0; i < clientList.length; i++) {
               var client = clientList[i];
-              if (client.url === '/' && 'focus' in client) {
+              if (client.url === event.notification.data.url && 'focus' in client) {
                   return client.focus();
               }
           }
           if (clients.openWindow) {
-              return clients.openWindow('/');
+              return clients.openWindow(event.notification.data.url);
           }
       })
+  );
+});
+
+self.addEventListener('push', function(event) {
+  const data = event.data.json();
+  const options = {
+      body: data.body,
+      icon: 'icon.png',
+      data: { url: data.url }
+  };
+  event.waitUntil(
+      self.registration.showNotification(data.title, options)
   );
 });
 
