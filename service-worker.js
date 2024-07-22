7d37b96222103ff706a05777f6cc236dc4c62181
nicealexei <alexei.frumkin@nice.com>
2024-07-22 16:01:50 -0400

    a

--- service-worker.js
+++ service-worker.js
@@ -84,25 +84,30 @@ self.addEventListener('message', event => {
   if (event.data && event.data.type === 'INIT_CALLBACK') {
     const callback = () => {
       console.log('Callback from Service Worker for MessageAddedIntoCase fired!');
-      // Check if there are any active clients
-      self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clients => {
-        if (clients.length === 0) {
-          // No active clients, PWA is in the background
-          console.log('No active clients, showing notification');
-          self.registration.showNotification('Hey there from Telco!', {
-            body: 'Agent sent you a new message.',
-            icon: 'assets/images/logo192.png',
-            badge: 'assets/images/logo192.png',
-            data: {
-              url: 'https://nicealexei.github.io/DSE-Product-Demo-Journey-Telco/'
-            }
-          });
-          console.log('Active clients not found, showing notification');
-        } 
-        else {
-          console.log('Active clients found, not showing notification');
-        }
-      });
+      if (!self.clients) {
+        console.error('Clients API is not available in the service worker.');
+        return;
+      }
+      self.clients.matchAll({ type: 'window', includeUncontrolled: true })
+        .then(clients => {
+          if (clients.length === 0) {
+            console.log('No active clients, showing notification');
+            self.registration.showNotification('Hey there from Telco!', {
+              body: 'Agent sent you a new message.',
+              icon: 'assets/images/logo192.png',
+              badge: 'assets/images/logo192.png',
+              data: {
+                url: 'https://nicealexei.github.io/DSE-Product-Demo-Journey-Telco/'
+              }
+            });
+            console.log('Active clients not found, showing notification');
+          } else {
+            console.log('Active clients found, not showing notification');
+          }
+        })
+        .catch(error => {
+          console.error('Error matching clients:', error);
+        });
     };
     event.ports[0].postMessage({ type: 'CALLBACK_RESPONSE', callback: callback.toString() });
   }
